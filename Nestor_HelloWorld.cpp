12b92529a87bb52b41177dfc4b745ef7a9a6ac36
Nestor Saavedra <31100486+saavfoxdev@users.noreply.github.com>
2017-10-06 12:28:11 -0700

    Squash and Merge saavfoxdev branch
    "Added some code." -saavfoxdev

---
+++ Nestor_HelloWorld.cpp
@@ -0,0 +1,12 @@
+/*
+Just a basic Hello World file for now
+When we have a good idea for a project, this will change
+*/
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    cout << "Hello World" << endl;
+
+}
